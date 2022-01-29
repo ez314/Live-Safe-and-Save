26343bc37971db8b8c793fa7a19f2c0e99d01556
Kevin Yao <ksyao2002@gmail.com>
2022-01-29 17:14:36 -0600

    update backend route for post request for object detection and readme

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from objectDetect import ObjectDetector
 app = Flask(__name__)
+det = ObjectDetector()
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/objectDetect",methods = ['POST'])
+def objectDetect():
+    try:
+        file = request.files['image']
+        expected = request.args['expected']
+        ret = det.runDetect(file)
+        if ret==expected:
+            return jsonify({'msg': 'success', 'result': True})
+        else:
+            return jsonify({'msg': 'success', 'result': False})
+    except:
+        return jsonify({'msg': '500 error'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- backend/objectDetect.py
+++ backend/objectDetect.py
@@ -1,23 +1,29 @@
-import os
 import clip
 import torch
 from PIL import Image
 
-
-
-# Load the model
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model, preprocess = clip.load('ViT-B/32', device)
-
-# Prepare the inputs
-image = preprocess(Image.open("nothing.jpg")).unsqueeze(0).to(device)
-text = clip.tokenize(["smoke detector", "lock", "fire extinguisher","random"]).to(device)
-
-with torch.no_grad():
-    image_features = model.encode_image(image)
-    text_features = model.encode_text(text)
+class ObjectDetector:
+    def __init__(self):
+        # Load the model
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model, self.preprocess = clip.load('ViT-B/32', self.device)
     
-    logits_per_image, logits_per_text = model(image, text)
-    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    def runDetect(self,filename,options = ["smoke detector", "lock", "fire extinguisher","nothing"]):
+        # Prepare the inputs
+        image = self.preprocess(Image.open(filename)).unsqueeze(0).to(self.device)
+        
+        text = clip.tokenize(options).to(self.device)
 
-print("Label probs:", probs)
+        with torch.no_grad():
+            image_features = self.model.encode_image(image)
+            text_features = self.model.encode_text(text)
+            
+            logits_per_image, logits_per_text = self.model(image, text)
+            probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+        
+        index_max = max(range(len(probs)), key=probs.__getitem__)
+
+        if probs[index_max]>0.7:
+            return options[index_max]
+        else:
+            return "nothing"
