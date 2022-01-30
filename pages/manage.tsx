b2169cf7cbb76751b81eb11dba1aff2a773b84be
Nicolas Andrew Burnett <nicolasandrewburnett@gmail.com>
2022-01-29 18:23:37 -0600

    feat(query): added automagic query params for manage page

--- pages/manage.tsx
+++ pages/manage.tsx
@@ -5,14 +5,9 @@ const Manage: NextPage = () => {
   return (
     <div className='mt-16 flex flex-col items-center text-custom-white-0'>
       <div className='text-5xl font-black'>Eric Zhang</div>
-      <div className='flex mt-4 flex-row items-center'>
-        <div className='flex flex-row items-center'>
-          <HomeSVG width={20} height={20} />
-          <div className='ml-2 text-lg cursor-pointer hover:underline'>Home Insurance</div>
-          <span>&nbsp;&gt; Manage Smoke Detectors</span>
-        </div>
-      </div>
-      <div className='mt-16 flex flex-col items-center'>
+      <div className='mt-2 text-lg font-semibold'>Manage Smoke Detectors</div>
+      <div className='mt-4 flex flex-col items-center'>
+        <div className='custom-btn'>Done</div>
         <div className='custom-btn'>Add smoke detector</div>
       </div>
     </div>
