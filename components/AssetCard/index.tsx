fd7a17f6201537c2c3c37b92d20f622f723894c6
Eric Zhang <36653830+ez314@users.noreply.github.com>
2022-01-30 00:20:59 -0600

    Change shadow to ring

--- components/AssetCard/index.tsx
+++ components/AssetCard/index.tsx
@@ -10,10 +10,11 @@ export interface AssetCardProps {
 }
 
 export default function AssetCard({ className, type, assetId, assetName, img, state }: AssetCardProps) {
-  const shadowColor = state == 'expired' ? 'shadow-custom-red' : state == 'needs-attention' ? 'shadow-custom-gold' : 'shadow-custom-green'
+  const ringColor = state == 'expired' ? 'ring-custom-red' : state == 'needs-attention' ? 'ring-custom-gold' : 'ring-custom-green'
+  console.log(ringColor, state)
   return (
     <Link href={`/manage?itemName=${type?.toLowerCase()}&assetId=${assetId}`} >
-      <div className={`cursor-pointer rounded-lg relative w-2/3 h-64 overflow-hidden shadow-lg ${shadowColor} ${className}`}>
+      <div className={`cursor-pointer rounded-lg relative w-2/3 h-64 overflow-hidden ring ${ringColor} ${className}`}>
         <img className='object-cover w-full h-full' src={img} alt={`Uploaded image for ${assetName}`}/>
         <div className="absolute w-full py-2.5 bottom-0 inset-x-0 bg-gray-600 text-white text-center leading-4 text-lg">
           {assetName}
